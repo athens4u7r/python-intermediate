8f5dda3381e891afe51235699791f5b35530b090
athens4u7r <46388529+athens4u7r@users.noreply.github.com>
2019-08-06 18:24:34 +0100

    Create portScan.py

---
+++ portScan.py
@@ -0,0 +1,22 @@
+from threading import Thread
+from socket import *
+import optparse
+
+def portScan(tgtHost, tgtPorts):
+  try:
+    tgtIP = gethostbyname
+
+def main():
+  parser = optparse.OptionParse("usage %prog -H <hostname> -p <port number(s)>")
+  parser.add_option("-H", dest= "tgtHost", type= "string", help= "specify the hostname")
+  parser.add_option("-p", dest= "tgtPort", type= "string", help= "specify the port number(s)")
+  
+  (options, args) = parser.parse_args()
+  
+  tgtHost = options.tgtHost
+  tgtPorts = str(options.tgtPort).split(',')
+  
+  if tgtHost == None and tgtPorts[0] == None:
+    print(parser.usage)
+    exit(0)
+  portScan(tgtHost, tgtPorts)
